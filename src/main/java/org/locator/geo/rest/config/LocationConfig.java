f9aed8b2a45c236ca4f8482c1ff11c946cfe97d3
Bijoy Paul <bijoypaul@Bijoys-MacBook-Pro.local>
2017-02-06 18:08:20 +0530

    cache controller added

--- src/main/java/org/locator/geo/rest/config/LocationConfig.java
+++ src/main/java/org/locator/geo/rest/config/LocationConfig.java
@@ -1,14 +1,17 @@
 package org.locator.geo.rest.config;
 
 import java.io.IOException;
+import org.locator.geo.rest.cache.LocationCache;
 import org.locator.geo.rest.dao.LocationDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+@EnableCaching
 @Configuration
 public class LocationConfig {
 	
@@ -20,7 +23,7 @@ public class LocationConfig {
 
 	@Bean
 	public GeoLocator provider() throws IOException{
-		return new LocationDao(maxDb, jdbcTemplate);
+		return new LocationCache(new LocationDao(maxDb, jdbcTemplate));
 	}
 
 }
