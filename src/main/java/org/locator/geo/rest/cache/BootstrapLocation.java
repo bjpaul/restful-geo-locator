a67d3e5b0b15d5e42477e01185c26e1cf184d84c
Bijoy Paul <bijoypaul@Bijoys-MacBook-Pro.local>
2017-02-08 17:07:28 +0530

    cache enabled

--- src/main/java/org/locator/geo/rest/cache/BootstrapLocation.java
+++ src/main/java/org/locator/geo/rest/cache/BootstrapLocation.java
@@ -26,26 +26,11 @@ public class BootstrapLocation implements InitializingBean{
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		
-		Thread thread = new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				try {
-					Thread.sleep(5000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				logger.info("Bootstrap :: Coordinates");
-				bootstrapCoordinates();
-				
-				logger.info("Bootstrap :: Keywords");
-				bootstrapKeywords();
-				
-			}
-		});
+		logger.info("Bootstrap :: Coordinates");
+		bootstrapCoordinates();
 		
-		thread.start();
+		logger.info("Bootstrap :: Keywords");
+		bootstrapKeywords();
 		
 	}
 
--- src/main/java/org/locator/geo/rest/config/LocationConfig.java
+++ src/main/java/org/locator/geo/rest/config/LocationConfig.java
@@ -1,18 +1,16 @@
 package org.locator.geo.rest.config;
 
 import java.io.IOException;
-import org.locator.geo.rest.cache.BootstrapLocation;
+
 import org.locator.geo.rest.cache.LocationCache;
 import org.locator.geo.rest.dao.LocationDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-@EnableCaching
 @Configuration
 public class LocationConfig {
 	
@@ -26,11 +24,5 @@ public class LocationConfig {
 	public GeoLocator provider() throws IOException{
 		return new LocationCache(new LocationDao(maxDb, jdbcTemplate));
 	}
-	
-	@Bean
-	public BootstrapLocation bootstrapLocation() throws IOException{
-		return new BootstrapLocation(provider(), jdbcTemplate);
-	}
-	
 
 }
